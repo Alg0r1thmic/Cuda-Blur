bfe860ae43ae4d309fa5753fbe7f28f84229e314
Kevin Salazar <kast46.15@gmail.com>
2018-12-11 19:01:44 -0500

    Update kernels.cu

--- kernels.cu
+++ kernels.cu
@@ -65,7 +65,7 @@ void blurr (unsigned char* imagen_ini, unsigned char* imagen_fin, int width, int
     float ms = 0;
     std::cout << "--------------\nTiempo de proceso: ";
     cudaEventElapsedTime(&ms, start, stop);
-    std::cout << ms/1000 <<" s\n--------------\n";
+    std::cout << ms/1000 <<" s\n--------------\n ";
 
     getError(cudaFree(cuda_input));
     getError(cudaFree(cuda_output));
